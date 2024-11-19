b498d2a23fef777bc0bb6fcb228e57d07c43662b
graywish <naingaunglinn369@gmail.com>
2024-11-20 05:37:50 +0630

    update: html with php

--- php-basic/index.php
+++ php-basic/index.php
@@ -1,6 +1,13 @@
-<?php
-
-$name = "naingaunglinn";
-echo "Hello $name";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <h1>
+        <?php
+            $name = "naingaunglinn";
+            $job = "web developer";
+            
+            echo "$name is $job";
+        ?>
+    </h1>
+</body>
+</html>
