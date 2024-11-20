370a1a8dd552bc3c9ab71f38e2d05e4afaa7eb8d
graywish <naingaunglinn369@gmail.com>
2024-11-21 05:26:49 +0630

    add: float functions

--- php-basic/index.php
+++ php-basic/index.php
@@ -1,9 +1,22 @@
 <?php
 
-/* Integers */
+/* Floats */
 
-$x = (int)200_200_200;
+$x = 9;
 
-echo $x;
+var_dump(floatval($x));
+
+$a = 0.23;
+$b = 1 - 0.77;
+
+var_dump($a, $b);
+
+if($a == $b) {
+    echo "Yes";
+} else {
+    echo "No";
+}
+echo $y;
+echo PHP_FLOAT_MAX * 2
 
 ?>
