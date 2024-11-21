41d170fc96087cb4b12262565d0ca33bb94d696d
naingaunglinn <naingaunglinn369@gmail.com>
2024-11-21 15:32:17 +0630

    add: array functions

--- php-basic/index.php
+++ php-basic/index.php
@@ -1,8 +1,43 @@
 <?php
 
-/* NULL */
+/* Arrays */
+$programmingLanguages = ['PHP', 'JAVA', 'JAVASCRIPT'];
 
-$x = null;
+echo "<pre>";
+print_r($programmingLanguages);
+echo "</pre>";
+echo count($programmingLanguages);
 
-var_dump(is_null($x));
-echo $x;
+array_push($programmingLanguages, 'C++', 'C', 'PYTHON');
+echo "<pre>";
+print_r($programmingLanguages);
+echo "</pre>";
+echo count($programmingLanguages);
+
+$programmingLanguages = [
+    'php' => [
+        'creator' => '',
+        'website' => 'www.php.net',
+        'extension' => '.php',
+        'isOpenSource' => true,
+        'versions' => [
+            ['version' => 8, 'releaseDate' => 'Nov 26, 2020'],
+            ['version' => 7.4, 'releaseDate' => 'Nov 28, 2019']
+        ]
+    ],
+    'python' => [
+        'creator' => '',
+        'website' => 'www.python.org',
+        'extension' => '.py',
+        'isOpenSource' => true,
+        'versions' => [
+            ['version' => 3.9, 'releaseDate' => 'Oct 5, 2020'],
+            ['version' => 7.4, 'releaseDate' => 'Oct 14, 2019']
+        ]
+    ]
+];
+
+echo "<pre>";
+print_r($programmingLanguages);
+echo "</pre>";
+echo count($programmingLanguages);
