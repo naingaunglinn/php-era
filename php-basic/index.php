8644da29ad0dce4facfccef68462782f304b4bf8
graywish <naingaunglinn369@gmail.com>
2024-11-21 20:50:01 +0630

    update: array functions

--- php-basic/index.php
+++ php-basic/index.php
@@ -1,43 +1,12 @@
 <?php
 
-/* Arrays */
-$programmingLanguages = ['PHP', 'JAVA', 'JAVASCRIPT'];
+/* Array */
 
-echo "<pre>";
-print_r($programmingLanguages);
-echo "</pre>";
-echo count($programmingLanguages);
+$x = 5;
 
-array_push($programmingLanguages, 'C++', 'C', 'PYTHON');
-echo "<pre>";
-print_r($programmingLanguages);
-echo "</pre>";
-echo count($programmingLanguages);
+var_dump((array) $x);
 
-$programmingLanguages = [
-    'php' => [
-        'creator' => '',
-        'website' => 'www.php.net',
-        'extension' => '.php',
-        'isOpenSource' => true,
-        'versions' => [
-            ['version' => 8, 'releaseDate' => 'Nov 26, 2020'],
-            ['version' => 7.4, 'releaseDate' => 'Nov 28, 2019']
-        ]
-    ],
-    'python' => [
-        'creator' => '',
-        'website' => 'www.python.org',
-        'extension' => '.py',
-        'isOpenSource' => true,
-        'versions' => [
-            ['version' => 3.9, 'releaseDate' => 'Oct 5, 2020'],
-            ['version' => 7.4, 'releaseDate' => 'Oct 14, 2019']
-        ]
-    ]
-];
+$array = ['a' => 5, 'b' => null];
 
-echo "<pre>";
-print_r($programmingLanguages);
-echo "</pre>";
-echo count($programmingLanguages);
+var_dump(array_key_exists('a', $array));
+var_dump(isset($array['a']));
