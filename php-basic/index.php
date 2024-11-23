a48951c7ec355ab2ec68157706cef6b7013f4384
graywish <naingaunglinn369@gmail.com>
2024-11-23 19:27:57 +0630

    add: condition statements

--- php-basic/index.php
+++ php-basic/index.php
@@ -1,5 +1,19 @@
 <?php
 
-/* Operator Precedence & Associativity */
-// follow this link 
-https://www.php.net/manual/en/language.operators.precedence.php
+/* Control Structures (if / else / elseif / else if)  */
+
+$score = 95;
+if($score >= 90){
+    echo 'A';
+    if($score >= 95){
+        echo '+';
+    }
+} else if($score >= 80 ) {
+    echo 'B';
+} else if($score >= 70) {
+    echo 'C';
+} else if($score >= 60) {
+    echo 'D';
+} else {
+    echo 'F';
+}
