9bd850ab591f0e35a555f5bb8b58b206ac262faf
graywish <naingaunglinn369@gmail.com>
2024-11-26 05:06:33 +0630

    update: function information

--- php-basic/index.php
+++ php-basic/index.php
@@ -1,20 +1,27 @@
 <?php
 
 /** Functions  **/
-
-// foo();
-// bar();
-// function foo(){
-//     echo 'foo';
-//     function bar(){
-//         echo 'bar';
+// declare(strict_types= 1);
+// function foo(int|float $a, int|float $b): int|float{
+//     if($a % 2 == 0){
+//         $a /= 2;
 //     }
+
+//     return $a * $b;
 // }
 
-/* Return Types and Strict Types */
-declare(strict_types= 1);
-function foo(): int|string{
-    return '1';
+// $x = 6.0;
+// $y = 7;
+
+// echo foo($x, $y);
+// var_dump($x, $y);
+
+function sum(...$numbers){
+    return array_sum($numbers);
+    var_dump($numbers); 
 }
 
-var_dump(foo());
+$x = 10;
+$y = 20;
+
+echo sum($x,$y,20,50);
