7bdd3e600aefa4e10c294b4c23ae057b522805b9
naingaunglinn <naingaunglinn369@gmail.com>
2024-11-27 07:46:57 +0630

    update: array functions

--- php-basic/index.php
+++ php-basic/index.php
@@ -9,3 +9,20 @@ $array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 $even = array_filter($array, fn($number, $key) => $number % 2 === 0, ARRAY_FILTER_USE_BOTH);
 
 prettyPrintArray($even, $info);
+
+$info = 'array_keys(array $keys, mixed $search_value, bool $strict = false): array';
+
+$array = ['a' => 5, 'b' => 10, 'c' => 15, 'd' => 5, 'e' => 10];
+
+$key = array_keys($array, 15, true);
+
+prettyPrintArray($key, $info);
+
+$info = 'array_map(callable|null $callable, array $array, array ...$arrays): array';
+
+$array1 = [1, 2, 3, 4, 5];
+$array2 = [6, 7, 8, 9, 10];
+
+$array = array_map(fn($number1, $number2) => $number1 * $number2, $array1, $array2);
+
+prettyPrintArray($array, $info);
