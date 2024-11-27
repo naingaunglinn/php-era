3520754ca2586728efa6709cc9a71e6f1210b231
naingaunglinn <naingaunglinn369@gmail.com>
2024-11-27 13:00:16 +0630

    update: array function

--- php-basic/index.php
+++ php-basic/index.php
@@ -26,3 +26,34 @@ $array2 = [6, 7, 8, 9, 10];
 $array = array_map(fn($number1, $number2) => $number1 * $number2, $array1, $array2);
 
 prettyPrintArray($array, $info);
+
+$info = 'array_merge(array ...$arrays): array';
+
+$array1 = [1, 2, 3];
+$array2 = [4, 5, 6];
+$array3 = [7, 8, 9];
+
+$merged = array_merge($array1, $array2, $array3);
+
+prettyPrintArray($merged, $info);
+
+$info = 'array_reduce(array $array, callable $callback, mixed $initialValue = null): mixed <br/>';
+
+$invoiceItems = [
+    ['price' => 9.99,  'qty' => 3, 'desc' => 'Item 1'],
+    ['price' => 10.99, 'qty' => 2, 'desc' => 'Item 2'],
+    ['price' => 30,    'qty' => 4, 'desc' => 'Item 3'],
+    ['price' => 158,   'qty' => 6, 'desc' => 'Item 4'],
+    ['price' => 3.56,  'qty' => 1, 'desc' => 'Item 5'],
+];
+
+$total = array_reduce(
+    $invoiceItems,
+    fn($sum, $item) => $sum + $item['qty'] * $item['price']
+);
+
+prettyPrintArray($total, $info);
+
+$array = ['a', 'b', 'c', 'D', 'E', 'ab', 'bc', 'cd', 'b', 'd'];
+
+
